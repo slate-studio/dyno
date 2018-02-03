2abb46b55d81ff2de775df34056140966987230c
Ruslan Sheremet <ruslan@slatestudio.com>
2018-02-03 16:00:39 +0200

    Refactoring - Refactoring of the exports functionality for autocomplete support - Fixed `engines` in package.json - Changed version to 0.16.2

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 'use strict'
 
+const {Server, server, errors, client, actions} = require('./lib/api')
+const {redis, mongodb, mongodbStaff, redisStaff} = require('./lib/db')
+const log = require('./lib/log')
+const {ConnectMsg} = require('./lib/msg')
+const utils = require('./src/utils')
+const RequestNamespace = require('./lib/RequestNamespace')
+
 module.exports = {
-  api:   require('./lib/api'),
-  db:    require('./lib/db'),
-  log:   require('./lib/log'),
-  msg:   require('./lib/msg'),
-  utils: require('./src/utils'),
-  RequestNamespace: require('./lib/requestNamespace')
+  api:      {Server, server, errors, client, actions},
+  db:       {redis, mongodb, mongodbStaff, redisStaff},
+  log,
+  msg:      require('./lib/msg'),
+  msgStaff: {ConnectMsg},
+  utils,
+  RequestNamespace
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = {
+exports = module.exports = {
   pluralize:                require('./pluralize'),
   request:                  require('./request'),
   // RequestNamespace:         require('./requestNamespace'),
