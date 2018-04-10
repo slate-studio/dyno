6a4226436d414bf167000cac541dc83606c5ea5f
Alexander Kravets <alex@slatestudio.com>
2018-04-10 18:38:48 +0300

    Add test/api structure

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   api:              require('./lib/api'),
   db:               require('./lib/db'),
   log:              require('./lib/log'),
+  oas:              require('./lib/oas'),
   jsonRequest:      require('./lib/jsonRequest'),
   RequestError:     require('./lib/requestError'),
   RequestNamespace: require('./lib/requestNamespace')
